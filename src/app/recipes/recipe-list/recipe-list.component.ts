f21cbb4f03753fd39477b34f2d191a7efd6cafd8
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2020-12-12 00:37:37 +0200

    53. Outputting a List of Recipes with ngfor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,6 +14,11 @@ export class RecipeListComponent implements OnInit {
       'This is a test',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
     ),
+    new Recipe(
+      'A Test Recipe',
+      'This is a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
   ];
 
   constructor() {}
