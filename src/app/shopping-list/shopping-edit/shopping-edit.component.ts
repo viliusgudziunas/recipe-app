7907f7439a4dd60cd74273a69117a79d0af9abd0
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2021-01-11 02:56:47 +0200

    89. Allowing the User to Add Ingredients to the Shopping List

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,7 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
 })
-export class ShoppingEditComponent {}
+export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAdd() {
+    this.ingredientAdded.emit(
+      new Ingredient(
+        this.nameInput.nativeElement.value,
+        this.amountInput.nativeElement.value
+      )
+    );
+
+    this.onClear();
+  }
+
+  onClear() {
+    this.nameInput.nativeElement.value = '';
+    this.amountInput.nativeElement.value = '';
+  }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -10,4 +10,8 @@ export class ShoppingListComponent {
     new Ingredient('Apple', '5'),
     new Ingredient('Tomato', '10'),
   ];
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
