b095993ca76a8f17aa843bfe6de6af3a30c2ec85
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2021-01-11 02:03:42 +0200

    85. Adding Navigation with Event Binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  currentPage = 'recipe';
+
+  onPageChanged(page: string) {
+    if (this.currentPage !== page) {
+      this.currentPage = page;
+    }
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  @Output() pageChanged = new EventEmitter<string>();
   collapsed = true;
+
+  onSelect(page: string) {
+    this.pageChanged.emit(page);
+  }
 }
